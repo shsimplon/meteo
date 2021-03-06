37af23eb8d5c28d703aa87127b301f38487db43b
samira <houacine.samira@gmail.com>
2021-03-06 22:09:23 +0100

    meteo geolocalisé

--- app.js
+++ app.js
@@ -1,15 +1,70 @@
 let temperature = document.getElementById('temperature_label');
 let divVille = document.getElementById('ville');
+let siel = document.getElementById('siel');
 
 let changer = document.getElementById('changer');
 
 
-let ville = "paris";
+let ville;
+// vide pour geolocaliser la méteo
+// dans certain cas lutilisateur a bloqué toute geaolocalisation sur son navigator donc:
+if ('geolocation' in navigator) {
+    /* la géolocalisation est disponible */
+    navigator.geolocation.watchPosition(function(position) {
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=0f526fc4c706001878f4fecba0e8ab16&units=metric';
+        console.log(url);
+        let config = {
+            method: "get"
+        };
 
-revevoirelaMeteo(ville);
+        fetch(url, config)
+            .then(function(response) {
+
+                return response.json()
+                    .then(function(data) {
+
+                        if (response.status == 400)
+                            console.log('erreur données requetes');
+                        else {
+
+                            let temp = data.main.temp;
+                            let vile = data.name;
+                            let apparence = data.main.humidity;
+
+                            siel.textContent = apparence;
+
+                            temperature.textContent = temp;
+
+                            divVille.textContent = vile;
+
+                        }
+
+                    })
+                    .catch(function(server_error) {
+                        console.log('error serveur')
+                            /* la géolocalisation n'est pas disponible */
+                    })
 
 
-// le fait d'appler la fonction ca permet d'avoir une température au début comme defaut
+            });
+
+
+    }, error, options);
+
+    var options = {
+        enableHighAccuracy: true
+            // l'application souhaite recevoir les meilleurs résultats possibles
+    }
+
+} else {
+    ville = "paris";
+    revevoirelaMeteo(ville)
+        /* la géolocalisation n'est pas disponible */
+
+
+}
+
+
 
 function revevoirelaMeteo(ville) {
 
@@ -36,11 +91,17 @@ function revevoirelaMeteo(ville) {
                         let pays = data.sys.country;
                         let vile = data.name;
 
+
                         temperature.textContent = temp;
 
                         divVille.textContent = vile;
 
+                        var newDiv = document.createElement("div");
+                        console.log(newDiv)
+                        newDiv.textContent = pays;
+                        // document.body.append(newDiv)
 
+                        divVille.prepend(newDiv);
 
                     }
 
@@ -56,6 +117,8 @@ function revevoirelaMeteo(ville) {
 
 }
 
+
+// le fait d'appler la fonction ca permet d'avoir une température au début comme defaut
 changer.addEventListener('click', function() {
 
     ville = prompt("rentrez une ville de votre choix");
@@ -66,3 +129,11 @@ changer.addEventListener('click', function() {
 
 
 });
+
+
+function error() {
+    ville = "paris";
+    revevoirelaMeteo(ville)
+        // en cas l 'utilisateur a bloqué sa localisation
+
+}
